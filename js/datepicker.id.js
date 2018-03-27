b4d94d4af09947b38b3bb19705f7b9bd54a3aaa2
farid <36753140+ridesign@users.noreply.github.com>
2018-03-27 13:16:26 +0700

    :"V

---
+++ js/datepicker.id.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['id'] = {
+    days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+    daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+    daysMin: ['Mu', 'Se', 'Sl', 'Ra', 'Ka', 'Ju', 'Sa'],
+    months: ['Januaryi','Februari','Maret','April','Mei','Juni', 'Juli','Agustus','September','Oktober','Nopember','Desember'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nop', 'Des'],
+    today: 'Hari Ini',
+    clear: 'Hapus',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
